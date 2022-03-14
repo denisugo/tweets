133483fd26bad4e82fa53d6166b9f523c2611668
Denis Ustiuzhanin <superden1320@gmail.com>
2022-03-14 15:49:08 +0100

    Initial commit

---
+++ Tweet.cs
@@ -0,0 +1,8 @@
+public struct Tweet
+{
+    public string Name { get; set; }
+    public string Nickname { get; set; }
+    public string Text { get; set; }
+    public string ImageSrc { get; set; }
+
+}
